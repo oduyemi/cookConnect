611b2efa0de5d4add0a550d3577501fde56a34d3
Yemi Oduyemi <yhermii@gmail.com>
2024-06-28 12:24:32 +0100

    update recipe form

--- src/components/CreateRecipe.jsx
+++ src/components/CreateRecipe.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { UserContext } from "../UserContext"; 
+import { UserContext } from "../UserContext";
 import { Box } from "@mui/material";
 import axios from "axios";
 
